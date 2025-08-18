206f5fca4975e753711e7e768f929088bd0523d3
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-08-18 22:57:20 +0000

    aula do dia 18/08/25, mas ainda desatualizado

--- ex01-express/src/index.js
+++ ex01-express/src/index.js
@@ -1,5 +1,11 @@
 import 'dotenv/config';
+import express from "express";
 
 console.log("FLAMENGO!");
-
 console.log(process.env.MY_SECRET);
+
+const app = express():
+
+app.listen(3000, () => {
+    console.log("Example app listening on port 3000!")
+    ; null.toString(); ))
