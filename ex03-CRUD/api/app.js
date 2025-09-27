2edc93655d49f6b9133ebac247e048558c907d6b
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-09-27 20:15:41 +0000

    iniciei o app.js

--- ex03-CRUD/api/app.js
+++ ex03-CRUD/api/app.js
@@ -1,16 +1,6 @@
 import express from 'express';
+import tarefasRouter from "./routes/tarefasRoutes.js";
 import dotenv from 'dotenv';
 
-dotenv.config();
-
 const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Servidor rodando com Express, Babel e Nodemon!');
-});
-
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-});
+const PORT = 3000;
