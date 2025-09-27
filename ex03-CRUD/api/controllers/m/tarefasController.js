133e905d07acf504db5eff21c2ac46f0a7109dfd
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-09-27 20:00:06 +0000

    tarefasController concluido

--- ex03-CRUD/api/controllers/m/tarefasController.js
+++ ex03-CRUD/api/controllers/m/tarefasController.js
@@ -1,9 +1,34 @@
-import * as TarefasModel from "../../models/tarefaModel.js";
+import * as TarefaModel from "../../models/tarefaModel.js";
 
 export function listarTarefas(req, res) {
-    const tarefa = TarefasModel.getTarefaById(req.params.id);
+    res.json(TarefaModel.getAllTarefas());
+}
+
+export function buscarTarefa(req, res) {
+    const tarefa = TarefaModel.getTarefaById(req.params.id);
     if (!tarefa) {
-        return res.status(404).json({ error: "A tarefa não foi encontrada"});
+        return res.status(404).json({ error: "A tarefa não foi encontrada" });
     }
     res.json(tarefa);
+}
+
+export function criarTarefa(req, res) {
+    const novaTarefa = TarefaModel.createTarefa(req.body);
+    res.status(201).json(novaTarefa);
+}
+
+export function atualizarTarefa(req, res) {
+    const tarefaAtualizada = TarefaModel.updateTarefa(req.params.id, req.body);
+    if (!tarefaAtualizada) {
+        return res.status(404).json({ error: "A tarefa não foi encontrada" });
+    }
+    res.json(tarefaAtualizada);
+}
+
+export function deletarTarefa(req, res) {
+    const tarefaDeletada = TarefaModel.deletarTarefa(req.params.id);
+    if (!tarefaDeletada) {
+        return res.status(404).json({ error: "A tarefa não foi encontrada" });
+    }
+    res.status(204).end();
 }
